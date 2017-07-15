45f4cc99500d151824ba529ab03eb19c8a120185
Daniel Atwood <sundevilgoalie13@gmail.com>
2017-07-15 14:16:18 -0400

    Add utility to load configuration.

---
+++ CommandCentral/Utilities/ConfigurationUtility.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandCentral.Utilities
+{
+    public static class ConfigurationUtility
+    {
+        public static IConfigurationRoot Configuration { get; private set; }
+
+        static ConfigurationUtility()
+        {
+            Configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+        }
+    }
+}
