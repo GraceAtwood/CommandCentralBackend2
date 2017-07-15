8745b9013df38857c643ce0c57e9877880b27530
Daniel Atwood <sundevilgoalie13@gmail.com>
2017-07-15 14:14:22 -0400

    Add Log management.

---
+++ CommandCentral/Log.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandCentral
+{
+    public static class Log
+    {
+        private static ILogger _logger;
+
+        public static ILogger LoggerInstance
+        {
+            get
+            {
+                return _logger;
+            }
+        }
+
+        public static void Initialize(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger("Command Central");
+        }
+
+        public static void Initialize()
+        {
+            _logger = new LoggerFactory().AddConsole().AddDebug().CreateLogger("Command Central");
+        }
+    }
+}
