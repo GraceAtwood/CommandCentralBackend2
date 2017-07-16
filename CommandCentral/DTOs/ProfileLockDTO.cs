5418f90ac19068e57d748c038587d8b04d12a590
Daniel Atwood <sundevilgoalie13@gmail.com>
2017-07-16 17:02:47 -0400

    Add profile lock DTOs

---
+++ CommandCentral/DTOs/ProfileLockDTO.cs
@@ -0,0 +1,18 @@
+﻿using CommandCentral.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation.Results;
+
+namespace CommandCentral.DTOs
+{
+    public class ProfileLockDTO
+    {
+        public Guid Id { get; set; }
+        public Guid Owner { get; set; }
+        public Guid LockedPerson { get; set; }
+        public DateTime SubmitTime { get; set; }
+        public TimeSpan MaxAge { get; set; }
+    }
+}
---
+++ CommandCentral/DTOs/ProfileLockQueryDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandCentral.DTOs
+{
+    public class ProfileLockQueryDTO
+    {
+        public Guid? Id { get; set; }
+        public Guid? Owner { get; set; }
+        public Guid? LockedPerson { get; set; }
+        public DateTime? RangeStart { get; set; }
+        public DateTime? RangeEnd { get; set; }
+    }
+}
