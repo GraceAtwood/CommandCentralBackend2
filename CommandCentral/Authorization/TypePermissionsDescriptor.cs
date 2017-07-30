cafecd6ceb0fc2559f161a4e6780dfca18e05419
Daniel Atwood <sundevilgoalie13@gmail.com>
2017-07-30 16:07:40 -0400

    Add overloads to CanEdit and CanReturn

--- CommandCentral/Authorization/TypePermissionsDescriptor.cs
+++ CommandCentral/Authorization/TypePermissionsDescriptor.cs
@@ -15,20 +15,57 @@ namespace CommandCentral.Authorization
         public Person Person { get; private set; }
         public Person Other { get; private set; }
 
-        public Dictionary<ChainsOfCommand, ChainOfCommandLevels> PersonHighestLevels { get; private set; }
+        private Dictionary<ChainsOfCommand, ChainOfCommandLevels> _personHighestLevels { get; set; }
 
         public TypePermissionsDescriptor(Person person, Person other)
         {
             this.Person = person;
             this.Other = other;
 
-            this.PersonHighestLevels = person.GetHighestAccessLevels();
+            this._personHighestLevels = person.GetHighestAccessLevels();
+        }
+
+        public bool CanReturn(string propertName)
+        {
+            if (!PermissionsCache.PermissionTypesCache.TryGetValue(typeof(T), out Dictionary<PropertyInfo, PropertyPermissionsCollection> props))
+                throw new Exception($"Your property info was declared by a type '{typeof(T).Name}' that we don't have permissions for.");
+
+            var propertyInfo = typeof(T).GetProperty(propertName) ??
+                throw new Exception($"Your property '{propertName}' does not exist on the type '{typeof(T).Name}'.");
+
+            if (!props.TryGetValue(propertyInfo, out PropertyPermissionsCollection permissions))
+                return false;
+
+            return CanReturn_Internal(permissions);
+        }
+
+        public bool CanReturn(PropertyInfo info)
+        {
+            if (info.DeclaringType != typeof(T))
+                throw new Exception($"The generic type T ({typeof(T).Name}) did not match the declaring type ({info.DeclaringType.Name}) of your property ({info.Name})!");
+
+            if (!PermissionsCache.PermissionTypesCache.TryGetValue(info.DeclaringType, out Dictionary<PropertyInfo, PropertyPermissionsCollection> props))
+                throw new Exception($"Your property info was declared by a type '{info.DeclaringType.Name}' that we don't have permissions for.");
+
+            if (!props.TryGetValue(info, out PropertyPermissionsCollection permissions))
+                return false;
+
+            return CanReturn_Internal(permissions);
         }
 
         public bool CanReturn<TValue>(Expression<Func<T, TValue>> selector)
         {
-            var permissions = PermissionsCache.PermissionTypesCache[typeof(T)][(PropertyInfo)selector.GetProperty()];
+            if (!PermissionsCache.PermissionTypesCache.TryGetValue(typeof(T), out Dictionary<PropertyInfo, PropertyPermissionsCollection> props))
+                throw new Exception($"Your type, {typeof(T).Name}, has no permissions declared about it.");
 
+            if (!props.TryGetValue((PropertyInfo)selector.GetProperty(), out PropertyPermissionsCollection permissions))
+                return false;
+
+            return CanReturn_Internal(permissions);
+        }
+
+        private bool CanReturn_Internal(PropertyPermissionsCollection permissions)
+        {
             if (permissions.CanReturnIfSelf && Other != null && Person.Equals(Other))
             {
                 return true;
@@ -39,7 +76,7 @@ namespace CommandCentral.Authorization
                 if (level.Value != ChainOfCommandLevels.None && Other == null)
                     return false;
 
-                if (PersonHighestLevels[level.Key] < level.Value)
+                if (_personHighestLevels[level.Key] < level.Value)
                     return false;
 
                 switch (level.Value)
@@ -79,10 +116,47 @@ namespace CommandCentral.Authorization
                 return default(TValue);
         }
 
+        public bool CanEdit(string propertName)
+        {
+            if (!PermissionsCache.PermissionTypesCache.TryGetValue(typeof(T), out Dictionary<PropertyInfo, PropertyPermissionsCollection> props))
+                throw new Exception($"Your property info was declared by a type '{typeof(T).Name}' that we don't have permissions for.");
+
+            var propertyInfo = typeof(T).GetProperty(propertName) ??
+                throw new Exception($"Your property '{propertName}' does not exist on the type '{typeof(T).Name}'.");
+
+            if (!props.TryGetValue(propertyInfo, out PropertyPermissionsCollection permissions))
+                return false;
+
+            return CanEdit_Internal(permissions);
+        }
+
+        public bool CanEdit(PropertyInfo info)
+        {
+            if (info.DeclaringType != typeof(T))
+                throw new Exception($"The generic type T ({typeof(T).Name}) did not match the declaring type ({info.DeclaringType.Name}) of your property ({info.Name})!");
+
+            if (!PermissionsCache.PermissionTypesCache.TryGetValue(info.DeclaringType, out Dictionary<PropertyInfo, PropertyPermissionsCollection> props))
+                throw new Exception($"Your property info was declared by a type '{info.DeclaringType.Name}' that we don't have permissions for.");
+
+            if (!props.TryGetValue(info, out PropertyPermissionsCollection permissions))
+                return false;
+
+            return CanReturn_Internal(permissions);
+        }
+
         public bool CanEdit<TValue>(Expression<Func<T, TValue>> selector)
         {
-            var permissions = PermissionsCache.PermissionTypesCache[typeof(T)][(PropertyInfo)selector.GetProperty()];
+            if (!PermissionsCache.PermissionTypesCache.TryGetValue(typeof(T), out Dictionary<PropertyInfo, PropertyPermissionsCollection> props))
+                throw new Exception($"Your type, {typeof(T).Name}, has no permissions declared about it.");
 
+            if (!props.TryGetValue((PropertyInfo)selector.GetProperty(), out PropertyPermissionsCollection permissions))
+                return false;
+
+            return CanEdit_Internal(permissions);
+        }
+
+        private bool CanEdit_Internal(PropertyPermissionsCollection permissions)
+        {
             if (permissions.CanNeverEdit)
                 return false;
 
@@ -96,7 +170,7 @@ namespace CommandCentral.Authorization
                 if (level.Value != ChainOfCommandLevels.None && Other == null)
                     return false;
 
-                if (PersonHighestLevels[level.Key] < level.Value)
+                if (_personHighestLevels[level.Key] < level.Value)
                     return false;
 
                 switch (level.Value)
