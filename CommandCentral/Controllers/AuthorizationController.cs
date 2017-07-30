6f896072397ee80527c5831e1bdc73e9be3326e3
CTI2(IDW) Angus McLean <anguslmm@gmail.com>
2017-07-30 18:09:13 -0400

    Almost finish ResolvedPermissionsDTO

--- CommandCentral/Controllers/AuthorizationController.cs
+++ CommandCentral/Controllers/AuthorizationController.cs
@@ -27,19 +27,17 @@ namespace CommandCentral.Controllers
             if (id.HasValue)
                 person = DBSession.Get<Person>(id.Value);
 
-            // var resolvedPermissions = new Authorization.ResolvedPermissions(User, person);
-
             var dto = new DTOs.ResolvedPermissionsDTO
             {
-                AccessibleSubmodules = ((Enums.SubModules[])Enum.GetValues(typeof(Enums.SubModules))).ToArray().Where(x => User.CanAccessSubmodules(new Enums.SubModules[] { x })).ToList(),
+                AccessibleSubmodules = ((Enums.SubModules[])Enum.GetValues(typeof(Enums.SubModules))).Where(x => User.CanAccessSubmodules(new Enums.SubModules[] { x })).ToList(),
                 EditablePermissionGroups = PermissionsCache.PermissionGroupsCache.Values.Where(x => User.CanEditPermissionGroups(new PermissionGroup[] { x })).Select(x => x.ToString()).ToList(),
-                FieldPermissions = new Dictionary<string, Dictionary<string, PropertyPermissionsDescriptor>> { { nameof(Person), PermissionsCache.PermissionTypesCache[typeof(Person)] } },
-                HighestLevels = resolvedPermissions.HighestLevels,
-                IsInChainOfCommand = resolvedPermissions.IsInChainOfCommand,
-                PermissionGroupNames = resolvedPermissions.PermissionGroups.Select(x => x.Name).ToList(),
-                PersonId = resolvedPermissions.Person.Id,
-                PersonResolvedAgainstId = resolvedPermissions.PersonResolvedAgainst?.Id,
-                ReturnableFieldsAtLevel = resolvedPermissions.ReturnableFieldsAtLevel
+                FieldPermissions = new Dictionary<string, Dictionary<string, PropertyPermissionsDescriptor>> { { nameof(Person), new TypePermissionsDescriptor<Person>(User, person).GetAllPermissions().ToDictionary(x => x.Property.Name, x => x)  } },
+                HighestLevels = User.GetHighestAccessLevels(),
+                IsInChainOfCommand = ((Enums.ChainsOfCommand[])Enum.GetValues(typeof(Enums.ChainsOfCommand))).ToDictionary(x=> x, x=> User.IsInChainOfCommand(person, new Enums.ChainsOfCommand[] { x })),
+                PermissionGroupNames = User.PermissionGroups.Select(x => x.Name).ToList(),
+                PersonId = User.Id,
+                PersonResolvedAgainstId = person.Id,
+                ReturnableFieldsAtLevel = resolvedPermissions.ReturnableFieldsAtLevel // TODO: Fix this
                     .ToDictionary(x => x.Key, x => x.Value.ToDictionary(y => y.Key.Name, y => y.Value.Select(z => z.Name).ToList()))          
             };
 
