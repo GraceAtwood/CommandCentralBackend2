305a56ddd30f9c8e38d1b4addfcdf7de1465f1bf
Daniel Atwood <sundevilgoalie13@gmail.com>
2017-07-30 22:48:57 -0400

    Add returnable fields at level to resolved permissions DTO

--- CommandCentral/Controllers/AuthorizationController.cs
+++ CommandCentral/Controllers/AuthorizationController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using CommandCentral.Framework;
 using CommandCentral.Entities;
 using CommandCentral.Authorization;
+using CommandCentral.Enums;
 
 namespace CommandCentral.Controllers
 {
@@ -27,18 +28,57 @@ namespace CommandCentral.Controllers
             if (id.HasValue)
                 person = DBSession.Get<Person>(id.Value);
 
+            var highestlevels = User.GetHighestAccessLevels();
+
+            var returnableFieldsAtLevel = new Dictionary<ChainOfCommandLevels, Dictionary<string, HashSet<string>>>();
+
+            foreach (var chainOfCommandLevel in ((ChainOfCommandLevels[])Enum.GetValues(typeof(ChainOfCommandLevels))).Where(x => x != ChainOfCommandLevels.None).OrderByDescending(x => x))
+            {
+                var result = new Dictionary<string, HashSet<string>>();
+                returnableFieldsAtLevel[chainOfCommandLevel] = result;
+
+                foreach (var typePermission in PermissionsCache.PermissionTypesCache)
+                {
+                    foreach (var propertyPermissions in typePermission.Value)
+                    {
+                        var type = typePermission.Key;
+                        var property = propertyPermissions.Key;
+                        var permissionCollection = propertyPermissions.Value;
+
+                        if (permissionCollection.LevelsRequiredToReturnForChainOfCommand.Any(level => (level.Value == ChainOfCommandLevels.None || level.Value == chainOfCommandLevel) && highestlevels[level.Key] >= level.Value))
+                        {
+                            if (!result.ContainsKey(type.Name))
+                                result.Add(type.Name, new HashSet<string> { property.Name });
+                            else
+                                result[type.Name].Add(property.Name);
+                        }
+                    }
+                }
+            }
+
             var dto = new DTOs.ResolvedPermissionsDTO
             {
-                AccessibleSubmodules = ((Enums.SubModules[])Enum.GetValues(typeof(Enums.SubModules))).Where(x => User.CanAccessSubmodules(new Enums.SubModules[] { x })).ToList(),
-                EditablePermissionGroups = PermissionsCache.PermissionGroupsCache.Values.Where(x => User.CanEditPermissionGroups(new PermissionGroup[] { x })).Select(x => x.ToString()).ToList(),
-                FieldPermissions = new Dictionary<string, Dictionary<string, PropertyPermissionsDescriptor>> { { nameof(Person), new TypePermissionsDescriptor<Person>(User, person).GetAllPermissions().ToDictionary(x => x.Property.Name, x => x)  } },
-                HighestLevels = User.GetHighestAccessLevels(),
-                IsInChainOfCommand = ((Enums.ChainsOfCommand[])Enum.GetValues(typeof(Enums.ChainsOfCommand))).ToDictionary(x=> x, x=> User.IsInChainOfCommand(person, new Enums.ChainsOfCommand[] { x })),
+                AccessibleSubmodules = ((SubModules[])Enum.GetValues(typeof(SubModules))).Where(x => User.CanAccessSubmodules(x)).ToList(),
+                EditablePermissionGroups = PermissionsCache.PermissionGroupsCache.Values.Where(x => User.CanEditPermissionGroups(x)).Select(x => x.ToString()).ToList(),
+                FieldPermissions = PermissionsCache.PermissionTypesCache.ToDictionary(x => x.Key.Name, x =>
+                {
+                    dynamic desc = Activator.CreateInstance(typeof(TypePermissionsDescriptor<>).MakeGenericType(x.Key), new[] { User, person });
+                    return ((IEnumerable<PropertyPermissionsDescriptor>)desc.GetAllPermissions())
+                        .ToDictionary(
+                            permissionsDescriptor => permissionsDescriptor.Property.Name, 
+                            permissionsDescriptor => 
+                                new DTOs.PropertyPermissionsDTO
+                                {
+                                    CanEdit = permissionsDescriptor.CanEdit,
+                                    CanReturn = permissionsDescriptor.CanReturn
+                                });
+                }),
+                HighestLevels = highestlevels,
+                IsInChainOfCommand = person == null ? null : ((ChainsOfCommand[])Enum.GetValues(typeof(ChainsOfCommand))).ToDictionary(x => x, chainOfCommand => User.IsInChainOfCommand(person, chainOfCommand)),
                 PermissionGroupNames = User.PermissionGroups.Select(x => x.Name).ToList(),
                 PersonId = User.Id,
-                PersonResolvedAgainstId = person.Id,
-                ReturnableFieldsAtLevel = resolvedPermissions.ReturnableFieldsAtLevel // TODO: Fix this
-                    .ToDictionary(x => x.Key, x => x.Value.ToDictionary(y => y.Key.Name, y => y.Value.Select(z => z.Name).ToList()))          
+                PersonResolvedAgainstId = person?.Id,
+                ReturnableFieldsAtLevel = returnableFieldsAtLevel.ToDictionary(x => x.Key, x => x.Value.ToDictionary(y => y.Key, y => y.Value.ToList()))
             };
 
             return Ok(dto);
