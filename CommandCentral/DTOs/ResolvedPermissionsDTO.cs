9ea721b61d765c4f1da5f2b72639b39c3fc69e89
Daniel Atwood <sundevilgoalie13@gmail.com>
2017-07-30 22:47:57 -0400

    Use DTO in resolved permissions

--- CommandCentral/DTOs/ResolvedPermissionsDTO.cs
+++ CommandCentral/DTOs/ResolvedPermissionsDTO.cs
@@ -14,7 +14,7 @@ namespace CommandCentral.DTOs
         public Guid PersonId { get; set; }
         public Guid? PersonResolvedAgainstId { get; set; }
         public bool IsSelf { get; set; }
-        public Dictionary<string, Dictionary<string, PropertyPermissionsDescriptor>> FieldPermissions { get; set; }
+        public Dictionary<string, Dictionary<string, PropertyPermissionsDTO>> FieldPermissions { get; set; }
         public Dictionary<ChainOfCommandLevels, Dictionary<string, List<string>>> ReturnableFieldsAtLevel { get; set; }
         public Dictionary<ChainsOfCommand, ChainOfCommandLevels> HighestLevels { get; set; }
         public Dictionary<ChainsOfCommand, bool> IsInChainOfCommand { get; set; }
