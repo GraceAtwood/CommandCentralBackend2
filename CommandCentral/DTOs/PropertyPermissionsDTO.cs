a66325b5c48c8d70374ae8cb9e23eb8cb6a4b36e
Daniel Atwood <sundevilgoalie13@gmail.com>
2017-07-30 22:47:29 -0400

    Add DTO of property permissions

---
+++ CommandCentral/DTOs/PropertyPermissionsDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandCentral.DTOs
+{
+    public class PropertyPermissionsDTO
+    {
+        public bool CanEdit { get; set; }
+        public bool CanReturn { get; set; }
+    }
+}
