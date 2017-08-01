6ca973fdb978567daae69ad41b6dc6fc3c4fd5ea
Daniel Atwood <sundevilgoalie13@gmail.com>
2017-08-01 17:36:26 -0400

    Implement account history event controller

---
+++ CommandCentral/DTOs/AccountHistoryEvent/Get.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandCentral.DTOs.AccountHistoryEvent
+{
+    public class Get
+    {
+        public Guid Id { get; set; }
+        public Guid AccountHistoryEventType { get; set; }
+        public DateTime EventTime { get; set; }
+        public Guid Person { get; set; }
+    }
+}
