6147809f7522e4abd9cd4b5a0bc05576c9419519
Daniel Atwood <sundevilgoalie13@gmail.com>
2017-08-01 16:55:30 -0400

    Add division DTOs

---
+++ CommandCentral/DTOs/Division/Update.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandCentral.DTOs.Division
+{
+    public class Update
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Guid Department { get; set; }
+    }
+}
