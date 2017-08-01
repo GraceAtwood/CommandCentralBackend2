20cb07027db20bfcf9836fcdf2bd8c99d708b621
Daniel Atwood <sundevilgoalie13@gmail.com>
2017-08-01 14:13:56 -0400

    Add physical address DTOs

---
+++ CommandCentral/DTOs/PhysicalAddress/Get.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandCentral.DTOs.PhysicalAddress
+{
+    public class Get : Update
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ CommandCentral/DTOs/PhysicalAddress/Update.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandCentral.DTOs.PhysicalAddress
+{
+    public class Update
+    {
+        public Guid Person { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string Country { get; set; }
+        public bool IsHomeAddress { get; set; }
+        public bool IsReleaseableOutsideCoC { get; set; }
+    }
+}
