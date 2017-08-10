373ee3fbbbd89c9dac6c32620deae5791612f17c
Daniel Atwood <sundevilgoalie13@gmail.com>
2017-08-09 22:09:35 -0400

    Implement constructor on person get dto

--- CommandCentral/Controllers/PersonController.cs
+++ CommandCentral/Controllers/PersonController.cs
@@ -38,43 +38,7 @@ namespace CommandCentral.Controllers
 
             var perms = User.GetFieldPermissions<Person>(person);
 
-            var dto = new DTOs.Person.Get
-            {
-                ADAMSTrainingDate = perms.GetSafeReturnValue(person, x => x.ADAMSTrainingDate),
-                Age = perms.GetSafeReturnValue(person, x => x.Age),
-                BilletAssignment = perms.GetSafeReturnValue(person, x => x.BilletAssignment)?.Id,
-                Command = perms.GetSafeReturnValue(person, x => x.Command)?.Id,
-                DateOfArrival = perms.GetSafeReturnValue(person, x => x.DateOfArrival),
-                DateOfBirth = perms.GetSafeReturnValue(person, x => x.DateOfBirth),
-                DateOfDeparture = perms.GetSafeReturnValue(person, x => x.DateOfDeparture),
-                Department = perms.GetSafeReturnValue(person, x => x.Department)?.Id,
-                Designation = perms.GetSafeReturnValue(person, x => x.Designation)?.Id,
-                Division = perms.GetSafeReturnValue(person, x => x.Division)?.Id,
-                DoDId = perms.GetSafeReturnValue(person, x => x.DoDId),
-                DutyStatus = perms.GetSafeReturnValue(person, x => x.DutyStatus)?.Id,
-                EAOS = perms.GetSafeReturnValue(person, x => x.EAOS),
-                Ethnicity = perms.GetSafeReturnValue(person, x => x.Ethnicity)?.Id,
-                FirstName = perms.GetSafeReturnValue(person, x => x.FirstName),
-                GTCTrainingDate = perms.GetSafeReturnValue(person, x => x.GTCTrainingDate),
-                HasCompletedAWARE = perms.GetSafeReturnValue(person, x => x.HasCompletedAWARE),
-                Id = perms.GetSafeReturnValue(person, x => x.Id),
-                JobTitle = perms.GetSafeReturnValue(person, x => x.JobTitle),
-                LastName = perms.GetSafeReturnValue(person, x => x.LastName),
-                MiddleName = perms.GetSafeReturnValue(person, x => x.MiddleName),
-                Paygrade = perms.GetSafeReturnValue(person, x => x.Paygrade)?.Id,
-                PRD = perms.GetSafeReturnValue(person, x => x.PRD),
-                ReligiousPreference = perms.GetSafeReturnValue(person, x => x.ReligiousPreference)?.Id,
-                Sex = perms.GetSafeReturnValue(person, x => x.Sex)?.Id,
-                Shift = perms.GetSafeReturnValue(person, x => x.Shift),
-                SSN = perms.GetSafeReturnValue(person, x => x.SSN),
-                Suffix = perms.GetSafeReturnValue(person, x => x.Suffix),
-                Supervisor = perms.GetSafeReturnValue(person, x => x.Supervisor),
-                UIC = perms.GetSafeReturnValue(person, x => x.UIC)?.Id,
-                WorkCenter = perms.GetSafeReturnValue(person, x => x.WorkCenter),
-                WorkRoom = perms.GetSafeReturnValue(person, x => x.WorkRoom)
-            };
-
-            return Ok(dto);
+            return Ok(new DTOs.Person.Get(person, perms));
         }
     }
 }
--- CommandCentral/DTOs/Person/Get.cs
+++ CommandCentral/DTOs/Person/Get.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommandCentral.Authorization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -39,5 +40,41 @@ namespace CommandCentral.DTOs.Person
         public DateTime? PRD { get; set; }
         public DateTime? DateOfDeparture { get; set; }
         public Guid? BilletAssignment { get; set; }
+
+        public Get(Entities.Person person, TypePermissionsDescriptor<Entities.Person> perms)
+        {
+            ADAMSTrainingDate = perms.GetSafeReturnValue(person, x => x.ADAMSTrainingDate);
+            Age = perms.GetSafeReturnValue(person, x => x.Age);
+            BilletAssignment = perms.GetSafeReturnValue(person, x => x.BilletAssignment)?.Id;
+            Command = perms.GetSafeReturnValue(person, x => x.Command)?.Id;
+            DateOfArrival = perms.GetSafeReturnValue(person, x => x.DateOfArrival);
+            DateOfBirth = perms.GetSafeReturnValue(person, x => x.DateOfBirth);
+            DateOfDeparture = perms.GetSafeReturnValue(person, x => x.DateOfDeparture);
+            Department = perms.GetSafeReturnValue(person, x => x.Department)?.Id;
+            Designation = perms.GetSafeReturnValue(person, x => x.Designation)?.Id;
+            Division = perms.GetSafeReturnValue(person, x => x.Division)?.Id;
+            DoDId = perms.GetSafeReturnValue(person, x => x.DoDId);
+            DutyStatus = perms.GetSafeReturnValue(person, x => x.DutyStatus)?.Id;
+            EAOS = perms.GetSafeReturnValue(person, x => x.EAOS);
+            Ethnicity = perms.GetSafeReturnValue(person, x => x.Ethnicity)?.Id;
+            FirstName = perms.GetSafeReturnValue(person, x => x.FirstName);
+            GTCTrainingDate = perms.GetSafeReturnValue(person, x => x.GTCTrainingDate);
+            HasCompletedAWARE = perms.GetSafeReturnValue(person, x => x.HasCompletedAWARE);
+            Id = perms.GetSafeReturnValue(person, x => x.Id);
+            JobTitle = perms.GetSafeReturnValue(person, x => x.JobTitle);
+            LastName = perms.GetSafeReturnValue(person, x => x.LastName);
+            MiddleName = perms.GetSafeReturnValue(person, x => x.MiddleName);
+            Paygrade = perms.GetSafeReturnValue(person, x => x.Paygrade)?.Id;
+            PRD = perms.GetSafeReturnValue(person, x => x.PRD);
+            ReligiousPreference = perms.GetSafeReturnValue(person, x => x.ReligiousPreference)?.Id;
+            Sex = perms.GetSafeReturnValue(person, x => x.Sex)?.Id;
+            Shift = perms.GetSafeReturnValue(person, x => x.Shift);
+            SSN = perms.GetSafeReturnValue(person, x => x.SSN);
+            Suffix = perms.GetSafeReturnValue(person, x => x.Suffix);
+            Supervisor = perms.GetSafeReturnValue(person, x => x.Supervisor);
+            UIC = perms.GetSafeReturnValue(person, x => x.UIC)?.Id;
+            WorkCenter = perms.GetSafeReturnValue(person, x => x.WorkCenter);
+            WorkRoom = perms.GetSafeReturnValue(person, x => x.WorkRoom);
+        }
     }
 }
