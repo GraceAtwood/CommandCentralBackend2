ef5165eb3d0285b7752758efc7962d7cef962c75
Daniel Atwood <sundevilgoalie13@gmail.com>
2017-08-16 11:45:57 -0400

    Add status period get dto constructor

--- CommandCentral/DTOs/StatusPeriod/Get.cs
+++ CommandCentral/DTOs/StatusPeriod/Get.cs
@@ -12,5 +12,18 @@ namespace CommandCentral.DTOs.StatusPeriod
         public Guid SubmittedBy { get; set; }
         public Guid LastModifiedBy { get; set; }
         public DateTime DateLastModified { get; set; }
+
+        public Get(Entities.Muster.StatusPeriod item)
+        {
+            DateSubmitted = item.DateSubmitted;
+            ExemptsFromWatch = item.ExemptsFromWatch;
+            Id = item.Id;
+            Person = item.Person.Id;
+            Range = item.Range;
+            Reason = item.AccountabilityType.Id;
+            SubmittedBy = item.SubmittedBy.Id;
+            DateLastModified = item.DateLastModified;
+            LastModifiedBy = item.LastModifiedBy.Id;
+        }
     }
 }
