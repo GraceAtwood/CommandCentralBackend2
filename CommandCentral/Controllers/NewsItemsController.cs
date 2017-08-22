f2f04b49a42e1cfbeabc996424a7d7d8e96a414c
Daniel Atwood <sundevilgoalie13@gmail.com>
2017-08-22 14:27:05 -0400

    Clean up news item controller

--- CommandCentral/Controllers/NewsItemsController.cs
+++ CommandCentral/Controllers/NewsItemsController.cs
@@ -18,127 +18,109 @@ namespace CommandCentral.Controllers
     {
         [HttpGet]
         [RequireAuthentication]
+        [ProducesResponseType(200, Type = typeof(List<DTOs.NewsItem.Get>))]
         public IActionResult Get()
         {
             var items = DBSession.QueryOver<NewsItem>().List();
 
-            return Ok(items.Select(x => new DTOs.NewsItem.Get
-            {
-                Id = x.Id,
-                Body = x.Body,
-                Title = x.Title,
-                CreationTime = x.CreationTime,
-                Creator = x.Creator.Id
-            }));
+            return Ok(items.Select(x => new DTOs.NewsItem.Get(x)));
         }
 
         [HttpGet("{id}")]
         [RequireAuthentication]
+        [ProducesResponseType(200, Type = typeof(DTOs.NewsItem.Get))]
         public IActionResult Get(Guid id)
         {
             var item = DBSession.Get<NewsItem>(id);
             if (item == null)
-                return NotFound();
+                return NotFoundParameter(id, nameof(id));
 
-            return Ok(new DTOs.NewsItem.Get
-            {
-                Id = item.Id,
-                Body = item.Body,
-                Title = item.Title,
-                CreationTime = item.CreationTime,
-                Creator = item.Creator.Id
-            });
+            return Ok(new DTOs.NewsItem.Get(item));
         }
 
         [HttpPost]
         [RequireAuthentication]
+        [ProducesResponseType(201, Type = typeof(DTOs.NewsItem.Get))]
         public IActionResult Post([FromBody]DTOs.NewsItem.Update dto)
         {
+            if (dto == null)
+                return BadRequestDTONull();
+
             if (!User.CanAccessSubmodules(SubModules.EditNews))
                 return Forbid();
 
+            var item = new NewsItem
+            {
+                Id = Guid.NewGuid(),
+                Title = dto.Title,
+                Body = dto.Body,
+                Creator = User,
+                CreationTime = CallTime
+            };
+
+            var result = item.Validate();
+            if (!result.IsValid)
+                return BadRequest(result.Errors.Select(x => x.ErrorMessage));
+
             using (var transaction = DBSession.BeginTransaction())
             {
-                var item = new NewsItem
-                {
-                    Id = Guid.NewGuid(),
-                    Title = dto.Title,
-                    Body = dto.Body,
-                    Creator = User,
-                    CreationTime = CallTime
-                };
-
-                var result = item.Validate();
-                if (!result.IsValid)
-                    return BadRequest(result.Errors.Select(x => x.ErrorMessage));
-
                 DBSession.Save(item);
                 transaction.Commit();
-
-                return CreatedAtAction(nameof(Get), new { id = item.Id }, new DTOs.NewsItem.Get
-                {
-                    Body = item.Body,
-                    CreationTime = item.CreationTime,
-                    Creator = item.Creator.Id,
-                    Id = item.Id,
-                    Title = item.Title
-                });
             }
-            
+
+            return CreatedAtAction(nameof(Get), new { id = item.Id }, new DTOs.NewsItem.Get(item));
         }
 
         [HttpPut("{id}")]
         [RequireAuthentication]
+        [ProducesResponseType(201, Type = typeof(DTOs.NewsItem.Get))]
         public IActionResult Put(Guid id, [FromBody]DTOs.NewsItem.Update dto)
         {
+            if (dto == null)
+                return BadRequestDTONull();
+            
             if (!User.CanAccessSubmodules(SubModules.EditNews))
                 return Forbid();
 
+            var item = DBSession.Get<NewsItem>(id);
+            if (item == null)
+                return NotFoundParameter(id, nameof(id));
+
+            item.Body = dto.Body;
+            item.Title = dto.Title;
+
+            var result = item.Validate();
+            if (!result.IsValid)
+                return BadRequest(result.Errors.Select(x => x.ErrorMessage));
+
             using (var transaction = DBSession.BeginTransaction())
             {
-                var item = DBSession.Get<NewsItem>(id);
-                if (item == null)
-                    return NotFound();
-
-                item.Body = dto.Body;
-                item.Title = dto.Title;
-
-                var result = item.Validate();
-                if (!result.IsValid)
-                    return BadRequest(result.Errors.Select(x => x.ErrorMessage));
-
                 DBSession.Update(item);
                 transaction.Commit();
-
-                return CreatedAtAction(nameof(Put), new { id = item.Id }, new DTOs.NewsItem.Get
-                {
-                    Body = item.Body,
-                    CreationTime = item.CreationTime,
-                    Creator = item.Creator.Id,
-                    Id = item.Id,
-                    Title = item.Title
-                });
             }
+
+            return CreatedAtAction(nameof(Put), new { id = item.Id }, new DTOs.NewsItem.Get(item));
         }
 
         [HttpDelete("{id}")]
         [RequireAuthentication]
+        [ProducesResponseType(204)]
         public IActionResult Delete(Guid id)
         {
             if (!User.CanAccessSubmodules(SubModules.EditNews))
                 return Forbid();
 
+            var item = DBSession.Get<NewsItem>(id);
+            if (item == null)
+                return NotFoundParameter(id, nameof(id));
+
             using (var transaction = DBSession.BeginTransaction())
             {
-                var item = DBSession.Get<NewsItem>(id);
-                if (item == null)
-                    return NotFound();
-
                 DBSession.Delete(item);
                 transaction.Commit();
-
-                return Ok();
             }
+
+            return NoContent();
         }
     }
 }
--- CommandCentral/DTOs/NewsItem/Get.cs
+++ CommandCentral/DTOs/NewsItem/Get.cs
@@ -8,7 +8,16 @@ namespace CommandCentral.DTOs.NewsItem
     public class Get : Update
     {
         public Guid Id { get; set; }
-        public Guid? Creator { get; set; }
+        public Guid Creator { get; set; }
         public DateTime? CreationTime { get; set; }
+
+        public Get(Entities.NewsItem item)
+        {
+            Id = item.Id;
+            Body = item.Body;
+            Title = item.Title;
+            CreationTime = item.CreationTime;
+            Creator = item.Creator.Id;
+        }
     }
 }
