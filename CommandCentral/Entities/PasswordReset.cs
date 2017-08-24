885a902c773c3d5efc0071dc60466e83aa95443a
CTI2(IDW) Angus McLean <anguslmm@gmail.com>
2017-08-24 10:57:27 -0400

    Create blank PasswordReset entity.

---
+++ CommandCentral/Entities/PasswordReset.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace CommandCentral.Entities
+{
+    public class PasswordReset : Entity
+    {
+        
+        
+        public override ValidationResult Validate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public class Validator : AbstractValidator<PasswordReset>
+        {
+            public Validator()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
