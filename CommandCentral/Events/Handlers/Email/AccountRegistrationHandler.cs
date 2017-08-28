644dbc736e4558309f47e5e639988a39424ac579
CTI2(IDW) Angus McLean <anguslmm@gmail.com>
2017-08-28 17:03:46 -0400

    Start event for account registration

---
+++ CommandCentral/Events/Handlers/Email/AccountRegistrationHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using CommandCentral.Authorization;
+using CommandCentral.Entities;
+using CommandCentral.Enums;
+using CommandCentral.Events.Args;
+using CommandCentral.Framework.Data;
+using NHibernate.Linq;
+
+namespace CommandCentral.Events.Handlers.Email
+{
+    public class AccountRegistrationHandler : IEventHandler
+    {
+        public AccountRegistrationHandler()
+        {
+            EventManager.AccountRegistered += OnAccountRegistered;
+        }
+
+        private void OnAccountRegistered(object sender, AccountRegistrationEventArgs e)
+        {
+            var groupsWithAccessToAdminModules = PermissionsCache.PermissionGroupsCache
+                .Values.Where(x => x.AccessibleSubmodules.Contains(SubModules.AdminTools))
+                .Select(x => x.Name)
+                .ToArray();
+
+            using (var session = SessionManager.GetCurrentSession())
+            {
+                var interestedPersons = session.Query<Person>()
+                    .Where(x => x.SubscribedEvents.ContainsKey(SubscribableEvents.PersonCreated))
+                    .Where(x => x.SubscribedEvents[SubscribableEvents.PersonCreated] == ChainOfCommandLevels.Division &&
+                                x.Division == e.AccountRegistration.Person.Division || 
+                                x.SubscribedEvents[SubscribableEvents.PersonCreated] == ChainOfCommandLevels.Department &&
+                                x.Department == e.AccountRegistration.Person.Department || 
+                                x.SubscribedEvents[SubscribableEvents.PersonCreated] == ChainOfCommandLevels.Command &&
+                                x.Command == e.AccountRegistration.Person.Command);
+                
+            }
+        }
+    }
+}
