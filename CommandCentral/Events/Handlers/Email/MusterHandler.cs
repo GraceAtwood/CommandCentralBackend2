24af705eaf52f6932e340240156158242e00d8cc
CTI2(IDW) Angus McLean <anguslmm@gmail.com>
2017-09-07 06:37:35 -0400

    Create MusterHandler

---
+++ CommandCentral/Events/Handlers/Email/MusterHandler.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using CommandCentral.Authorization;
+using CommandCentral.Entities;
+using CommandCentral.Enums;
+using CommandCentral.Events.Args;
+using CommandCentral.Framework.Data;
+using NHibernate.Linq;
+
+namespace CommandCentral.Events.Handlers.Email
+{
+    public class MusterHandler : IEventHandler
+    {
+        public MusterHandler()
+        {
+            EventManager.MusterOpened += OnMusterOpened;
+            EventManager.MusterFinalized += OnMusterFinalized;
+            EventManager.MusterReopened += OnMusterReopened;
+            EventManager.MusterEntrySubmitted += OnMusterEntrySubmitted;
+            EventManager.MusterEntryDeleted += OnMusterEntryDeleted;
+        }
+
+        private void OnMusterOpened(object sender, MusterCycleEventArgs e)
+        {
+            /*var musterPermissionsGroup = PermissionsCache.PermissionGroupsCache["Muster"].;
+            var interestedPerson = SessionManager.GetCurrentSession().Query<Person>()
+                .Where(x => x.PermissionGroups.Any(group => group));*/
+        }
+
+        private void OnMusterFinalized(object sender, MusterCycleEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void OnMusterReopened(object sender, MusterCycleEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void OnMusterEntrySubmitted(object sender, MusterEntryEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void OnMusterEntryDeleted(object sender, MusterEntryEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
