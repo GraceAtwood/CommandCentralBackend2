8916ab35b1622c7f4e0fb020736208d78c945f4f
Daniel Atwood <sundevilgoalie13@gmail.com>
2017-10-03 01:20:47 -0400

    Fix formatting

--- CommandCentral/Email/Templates.cs
+++ CommandCentral/Email/Templates.cs
@@ -1,4 +1,4 @@
-﻿﻿using CommandCentral.Email.Models;
+﻿using CommandCentral.Email.Models;
 
 namespace CommandCentral.Email
 {
@@ -10,66 +10,79 @@ namespace CommandCentral.Email
         /// <summary>
         /// The template meant to be used for the correspondence completed email.
         /// </summary>
-        public static readonly CCEmailTemplate<CorrespondenceGeneric> CorrespondenceCompletedTemplate = new CCEmailTemplate<CorrespondenceGeneric>("CorrespondenceCompleted.cshtml");
-        
+        public static readonly CCEmailTemplate<CorrespondenceGeneric> CorrespondenceCompletedTemplate =
+            new CCEmailTemplate<CorrespondenceGeneric>("CorrespondenceCompleted.cshtml");
+
         /// <summary>
         /// The template meant to be used for the correspondence created email.
         /// </summary>
-        public static readonly CCEmailTemplate<CorrespondenceGeneric> CorrespondenceCreatedTemplate = new CCEmailTemplate<CorrespondenceGeneric>("CorrespondenceCreated.cshtml");
-        
+        public static readonly CCEmailTemplate<CorrespondenceGeneric> CorrespondenceCreatedTemplate =
+            new CCEmailTemplate<CorrespondenceGeneric>("CorrespondenceCreated.cshtml");
+
         /// <summary>
         /// The template meant to be used for the correspondence deleted email.
         /// </summary>
-        public static readonly CCEmailTemplate<CorrespondenceGeneric> CorrespondenceDeletedTemplate = new CCEmailTemplate<CorrespondenceGeneric>("CorrespondenceDeleted.cshtml");
-        
+        public static readonly CCEmailTemplate<CorrespondenceGeneric> CorrespondenceDeletedTemplate =
+            new CCEmailTemplate<CorrespondenceGeneric>("CorrespondenceDeleted.cshtml");
+
         /// <summary>
         /// The template meant to be used for the correspondence modified email.
         /// </summary>
-        public static readonly CCEmailTemplate<CorrespondenceGeneric> CorrespondenceModifiedTemplate = new CCEmailTemplate<CorrespondenceGeneric>("CorrespondenceModified.cshtml");
-        
+        public static readonly CCEmailTemplate<CorrespondenceGeneric> CorrespondenceModifiedTemplate =
+            new CCEmailTemplate<CorrespondenceGeneric>("CorrespondenceModified.cshtml");
+
         /// <summary>
         /// The template meant to be used for the correspondence email when it is routed to a new person.
         /// </summary>
-        public static readonly CCEmailTemplate<CorrespondenceRouted> CorrespondenceRoutedTemplate = new CCEmailTemplate<CorrespondenceRouted>("CorrespondenceRouted.cshtml");
-        
+        public static readonly CCEmailTemplate<CorrespondenceRouted> CorrespondenceRoutedTemplate =
+            new CCEmailTemplate<CorrespondenceRouted>("CorrespondenceRouted.cshtml");
+
         /// <summary>
         /// The template meant to be used in the email for when a corr item is shared to new people.
         /// </summary>
-        public static readonly CCEmailTemplate<CorrespondenceShared> CorrespondenceSharedTemplate = new CCEmailTemplate<CorrespondenceShared>("CorrespondenceShared.cshtml"); 
-        
+        public static readonly CCEmailTemplate<CorrespondenceShared> CorrespondenceSharedTemplate =
+            new CCEmailTemplate<CorrespondenceShared>("CorrespondenceShared.cshtml");
+
         /// <summary>
         /// The template meant to be used in the email for when a corr item unshared from a person.
         /// </summary>
-        public static readonly CCEmailTemplate<CorrespondenceGeneric> CorrespondenceUnsharedTemplate = new CCEmailTemplate<CorrespondenceGeneric>("CorrespondenceUnshared.cshtml");
+        public static readonly CCEmailTemplate<CorrespondenceGeneric> CorrespondenceUnsharedTemplate =
+            new CCEmailTemplate<CorrespondenceGeneric>("CorrespondenceUnshared.cshtml");
 
         /// <summary>
         /// The template meant to be used for when a corr item's review is modified.
         /// </summary>
-        public static readonly CCEmailTemplate<CorrespondenceGeneric> ReviewModifiedTemplate = new CCEmailTemplate<CorrespondenceGeneric>("ReviewModified.cshtml");
+        public static readonly CCEmailTemplate<CorrespondenceGeneric> ReviewModifiedTemplate =
+            new CCEmailTemplate<CorrespondenceGeneric>("ReviewModified.cshtml");
 
         /// <summary>
         /// The template to be used when a review of a correspondence chain is deleted.
         /// </summary>
-        public static readonly CCEmailTemplate<CorrespondenceGeneric> ReviewDeletedTemplate = new CCEmailTemplate<CorrespondenceGeneric>("ReviewDeleted.cshtml");
-        
+        public static readonly CCEmailTemplate<CorrespondenceGeneric> ReviewDeletedTemplate =
+            new CCEmailTemplate<CorrespondenceGeneric>("ReviewDeleted.cshtml");
+
         /// <summary>
         /// The template meant to be used in the email for when an account is finished with registration.
         /// </summary>
-        public static readonly CCEmailTemplate<AccountRegistered> AccountRegisteredTemplate = new CCEmailTemplate<AccountRegistered>("AccountRegistered.cshtml");
-        
+        public static readonly CCEmailTemplate<AccountRegistered> AccountRegisteredTemplate =
+            new CCEmailTemplate<AccountRegistered>("AccountRegistered.cshtml");
+
         /// <summary>
         /// The template meant to be used in the email for when account registration is started.
         /// </summary>
-        public static readonly CCEmailTemplate<RegistrationStarted> RegistrationStartedTemplate = new CCEmailTemplate<RegistrationStarted>("RegistrationStarted.cshtml");
-        
+        public static readonly CCEmailTemplate<RegistrationStarted> RegistrationStartedTemplate =
+            new CCEmailTemplate<RegistrationStarted>("RegistrationStarted.cshtml");
+
         /// <summary>
         /// The template meant to be used in the email for when account registration is started.
         /// </summary>
-        public static readonly CCEmailTemplate<CollateralDeleted> CollateralDeletedTemplate = new CCEmailTemplate<CollateralDeleted>("CollateralDeleted.cshtml");
+        public static readonly CCEmailTemplate<CollateralDeleted> CollateralDeletedTemplate =
+            new CCEmailTemplate<CollateralDeleted>("CollateralDeleted.cshtml");
 
         /// <summary>
         /// The template meant to be used in the email for when a collateral duty is assigned.
         /// </summary>
-        public static CCEmailTemplate<CollateralMembershipCreated> CollateralMembershipCreated = new CCEmailTemplate<CollateralMembershipCreated>("CollateralMembershipCreated.cshtml");
+        public static CCEmailTemplate<CollateralMembershipCreated> CollateralMembershipCreated =
+            new CCEmailTemplate<CollateralMembershipCreated>("CollateralMembershipCreated.cshtml");
     }
 }
