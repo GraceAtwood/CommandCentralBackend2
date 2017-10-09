54b95e13596c09fd0d86e3a8c47a70ef70c65e11
Daniel Atwood <sundevilgoalie13@gmail.com>
2017-10-08 21:06:38 -0400

    Modify reference list based on changes on master

--- CommandCentral/Entities/ReferenceLists/CFSRequestType.cs
+++ CommandCentral/Entities/ReferenceLists/CFSRequestType.cs
@@ -10,36 +10,10 @@ namespace CommandCentral.Entities.ReferenceLists
     public class CFSRequestType : ReferenceListItemBase
     {
         /// <summary>
-        /// Validates this object.
-        /// </summary>
-        /// <returns></returns>
-        public override ValidationResult Validate()
-        {
-            return new Validator().Validate(this);
-        }
-
-        /// <summary>
         /// Maps this object to the database.
         /// </summary>
         public class CFSRequestTypeMapping : SubclassMap<CFSRequestType>
         {
-        }
-
-        /// <summary>
-        /// Validates this object.
-        /// </summary>
-        public class Validator : AbstractValidator<CFSRequestType>
-        {
-            /// <summary>
-            /// Validates this object.
-            /// </summary>
-            public Validator()
-            {
-                RuleFor(x => x.Description).Length(0, 255)
-                    .WithMessage("The description of a request type must be no more than 255 characters.");
-                RuleFor(x => x.Value).NotEmpty()
-                    .WithMessage("The value must not be null.");
-            }
         }
     }
 }
