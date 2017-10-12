9072497a79c075c263c2421fff280d5e19e4d5e9
Daniel Atwood <sundevilgoalie13@gmail.com>
2017-10-12 15:31:53 -0400

    Implement entire request controller

--- CommandCentral/Controllers/CFS/CFSRequestsController.cs
+++ CommandCentral/Controllers/CFS/CFSRequestsController.cs
@@ -47,5 +47,94 @@ namespace CommandCentral.Controllers.CFS
 
             return Ok(results);
         }
+
+        [HttpGet("{id}")]
+        [RequireAuthentication]
+        [ProducesResponseType(200, Type = typeof(DTOs.CFSRequest.Get))]
+        public IActionResult Get(Guid id)
+        {
+            var item = DBSession.Get<Request>(id);
+            if (item == null)
+                return NotFoundParameter(id, nameof(id));
+
+            if (!User.IsInChainOfCommand(item.Person, ChainsOfCommand.CommandFinancialSpecialist))
+                return Forbid("You must be in the CFS chain of command to view this request.");
+
+            return Ok(new DTOs.CFSRequest.Get(item));
+        }
+
+        [HttpPost]
+        [RequireAuthentication]
+        [ProducesResponseType(201, Type = typeof(DTOs.CFSRequest.Post))]
+        public IActionResult Post([FromBody] DTOs.CFSRequest.Post dto)
+        {
+            if (dto == null)
+                return BadRequestDTONull();
+
+            var person = DBSession.Get<Person>(dto.Person);
+            if (person == null)
+                return NotFoundParameter(dto.Person, nameof(dto.Person));
+
+            if (User != person && !User.IsInChainOfCommand(person, ChainsOfCommand.CommandFinancialSpecialist))
+                return Forbid("You must be in the CFS chain of command for this person.");
+
+            var item = new Request
+            {
+                Id = Guid.NewGuid(),
+                TimeSubmitted = CallTime,
+                RequestType = dto.RequestType,
+                Person = person
+            };
+
+            DBSession.Save(item);
+            CommitChanges();
+
+            return CreatedAtAction(nameof(Get), new {id = item.Id}, new DTOs.CFSRequest.Get(item));
+        }
+
+        [HttpPut("{id}")]
+        [RequireAuthentication]
+        [ProducesResponseType(201, Type = typeof(DTOs.CFSRequest.Get))]
+        public IActionResult Put(Guid id, [FromBody] DTOs.CFSRequest.Put dto)
+        {
+            if (dto == null)
+                return BadRequestDTONull();
+
+            var item = DBSession.Get<Request>(id);
+            if (item == null)
+                return NotFoundParameter(id, nameof(id));
+
+            if (!User.IsInChainOfCommand(item.Person, ChainsOfCommand.CommandFinancialSpecialist))
+                return Forbid("You must be in the CFS chain of command for this person.");
+
+            var claimedBy = DBSession.Get<Person>(dto.ClaimedBy);
+            if (claimedBy == null)
+                return NotFoundParameter(dto.ClaimedBy, nameof(dto.ClaimedBy));
+
+            item.ClaimedBy = claimedBy;
+            item.IsClaimed = true;
+
+            CommitChanges();
+
+            return CreatedAtAction(nameof(Get), new {id = item.Id}, new DTOs.CFSRequest.Get(item));
+        }
+
+        [HttpDelete("{id}")]
+        [RequireAuthentication]
+        [ProducesResponseType(204)]
+        public IActionResult Delete(Guid id)
+        {
+            var item = DBSession.Get<Request>(id);
+            if (item == null)
+                return NotFoundParameter(id, nameof(id));
+
+            if (!User.IsInChainOfCommand(item.Person, ChainsOfCommand.CommandFinancialSpecialist))
+                return Forbid("You must be in this person's CFS chain of command.");
+
+            DBSession.Delete(item);
+            CommitChanges();
+
+            return NoContent();
+        }
     }
 }
