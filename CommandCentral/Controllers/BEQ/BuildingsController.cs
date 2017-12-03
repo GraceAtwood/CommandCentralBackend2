178324c1417bab77a0f18b59d8a8d85a48515cf9
Daniel Atwood <sundevilgoalie13@gmail.com>
2017-12-03 17:33:12 -0500

    Comment and review

--- CommandCentral/Controllers/BEQ/BuildingsController.cs
+++ CommandCentral/Controllers/BEQ/BuildingsController.cs
@@ -12,14 +12,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CommandCentral.Controllers.BEQ
 {
+    /// <summary>
+    /// Provides access to the buildings resource.  Requires access, in most cases, to the BEQ chain of command.
+    /// </summary>
     public class BuildingsController : CommandCentralController
     {
         /// <summary>
-        /// 
+        /// Queries the buildings collection.
         /// </summary>
-        /// <param name="description"></param>
-        /// <param name="name"></param>
-        /// <param name="command"></param>
+        /// <param name="description">A string query for the description of a building.</param>
+        /// <param name="name">A string query for the name of a building.</param>
+        /// <param name="command">A command query for the command of a building.</param>
         /// <returns></returns>
         [HttpGet]
         [ProducesResponseType(200, Type = typeof(List<DTOs.Building.Get>))]
@@ -41,6 +44,11 @@ namespace CommandCentral.Controllers.BEQ
             return Ok(results);
         }
 
+        /// <summary>
+        /// Retrieves the building identified by the given id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpGet("{id}")]
         [ProducesResponseType(200, Type = typeof(DTOs.Building.Get))]
         public IActionResult Get(Guid id)
@@ -55,6 +63,11 @@ namespace CommandCentral.Controllers.BEQ
             return Ok(new DTOs.Building.Get(building));
         }
 
+        /// <summary>
+        /// Creates a new building.
+        /// </summary>
+        /// <param name="dto">Contains the information needed to create a new building.</param>
+        /// <returns></returns>
         [HttpPost]
         [ProducesResponseType(201, Type = typeof(DTOs.Building.Get))]
         public IActionResult Post([FromBody] DTOs.Building.Post dto)
@@ -64,7 +77,7 @@ namespace CommandCentral.Controllers.BEQ
 
             var command = DBSession.Get<Command>(dto.Command);
             if (command == null)
-                return NotFoundParameter(command, nameof(command));
+                return NotFoundParameter(dto.Command, nameof(command));
 
             var building = new Building
             {
@@ -88,6 +101,12 @@ namespace CommandCentral.Controllers.BEQ
             return CreatedAtAction(nameof(Get), new {id = building.Id}, new DTOs.Building.Get(building));
         }
 
+        /// <summary>
+        /// Updates a building.
+        /// </summary>
+        /// <param name="id">The id of the building to update.</param>
+        /// <param name="dto">A dto containing all of the information needed to modify a building.</param>
+        /// <returns></returns>
         [HttpPut("{id}")]
         [ProducesResponseType(201, Type = typeof(DTOs.Building.Get))]
         public IActionResult Put(Guid id, [FromBody] DTOs.Building.Put dto)
@@ -115,6 +134,11 @@ namespace CommandCentral.Controllers.BEQ
             return CreatedAtAction(nameof(Get), new {id = building.Id}, new DTOs.Building.Get(building));
         }
 
+        /// <summary>
+        /// Deletes the given building.
+        /// </summary>
+        /// <param name="id">The id of the building to delete.</param>
+        /// <returns></returns>
         [HttpDelete("{id}")]
         [ProducesResponseType(204)]
         public IActionResult Delete(Guid id)
