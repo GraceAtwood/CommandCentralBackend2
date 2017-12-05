91a55cf904399add4918b5771165652e374babff
Daniel Atwood <sundevilgoalie13@gmail.com>
2017-12-04 21:23:42 -0500

    Comment news items controller

--- CommandCentral/Controllers/NewsItemsController.cs
+++ CommandCentral/Controllers/NewsItemsController.cs
@@ -19,11 +19,11 @@ namespace CommandCentral.Controllers
         /// <summary>
         /// Queries the news items collection.
         /// </summary>
-        /// <param name="creator"></param>
-        /// <param name="title"></param>
-        /// <param name="body"></param>
-        /// <param name="creationTime"></param>
-        /// <param name="limit"></param>
+        /// <param name="creator">A person query for the creator of a news item.</param>
+        /// <param name="title">A string query for the title.</param>
+        /// <param name="body">A string query for the body.</param>
+        /// <param name="creationTime">A date time range query for the creation time.</param>
+        /// <param name="limit">[Optional][Default = 1000] Instructs the service to return no more than this number of results.</param>
         /// <returns></returns>
         [HttpGet]
         [ProducesResponseType(typeof(List<DTOs.NewsItem.Get>), 200)]
@@ -51,6 +51,11 @@ namespace CommandCentral.Controllers
             return Ok(results);
         }
 
+        /// <summary>
+        /// Retrieves a news item.
+        /// </summary>
+        /// <param name="id">The id of the news item to retrieve.</param>
+        /// <returns></returns>
         [HttpGet("{id}")]
         [ProducesResponseType(200, Type = typeof(DTOs.NewsItem.Get))]
         public IActionResult Get(Guid id)
@@ -62,6 +67,11 @@ namespace CommandCentral.Controllers
             return Ok(new DTOs.NewsItem.Get(item));
         }
 
+        /// <summary>
+        /// Creates a news item.
+        /// </summary>
+        /// <param name="dto">A dto containing the information needed to create a news item.</param>
+        /// <returns></returns>
         [HttpPost]
         [ProducesResponseType(201, Type = typeof(DTOs.NewsItem.Get))]
         public IActionResult Post([FromBody] DTOs.NewsItem.Update dto)
@@ -92,6 +102,12 @@ namespace CommandCentral.Controllers
             return CreatedAtAction(nameof(Get), new {id = item.Id}, new DTOs.NewsItem.Get(item));
         }
 
+        /// <summary>
+        /// Modifies a news item.
+        /// </summary>
+        /// <param name="id">The id of the news item to modify.</param>
+        /// <param name="dto">A dto containing the information needed to modify a news item.</param>
+        /// <returns></returns>
         [HttpPut("{id}")]
         [ProducesResponseType(201, Type = typeof(DTOs.NewsItem.Get))]
         public IActionResult Put(Guid id, [FromBody] DTOs.NewsItem.Update dto)
@@ -118,6 +134,11 @@ namespace CommandCentral.Controllers
             return CreatedAtAction(nameof(Put), new {id = item.Id}, new DTOs.NewsItem.Get(item));
         }
 
+        /// <summary>
+        /// Deletes a news item.
+        /// </summary>
+        /// <param name="id">The id of the news item to delete.</param>
+        /// <returns></returns>
         [HttpDelete("{id}")]
         [ProducesResponseType(204)]
         public IActionResult Delete(Guid id)
